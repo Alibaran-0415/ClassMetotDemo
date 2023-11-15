195960d8fb63676fa14b945f805ced19f3bb51ed
Muhammet Ali Baran <alibaran0415@gmail.com>
2023-11-15 17:01:54 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName+" adlı müşteri eklendi!");
+            Console.WriteLine("----------------------");
+        }
+
+        public void List(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName+" adlı müşterinin bilgileri");
+            Console.WriteLine("Müşteri Id:{0}",customer.CustomerId);
+            Console.WriteLine("Müşteri Adı:{0}",customer.FirstName);
+            Console.WriteLine("Müşteri Soyadı:{0}",customer.LastName);
+            Console.WriteLine("Müşteri Adresi:{0}",customer.Address);
+            Console.WriteLine("Müşteri Tc:{0}",customer.TcNo);
+            Console.WriteLine("----------------------");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName+" adlı müşteri veritabanından silindi.");
+            Console.WriteLine("----------------------");
+        }
+
+        public void Update(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName+" adlı müşterinin verileri güncellendi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.CustomerId = 1;
+            customer1.FirstName = "Muhammet";
+            customer1.LastName = "BARAN";
+            customer1.Address = "Kahraman Sokak No:32 Daire:7";
+            customer1.TcNo = "23423423524";
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.List(customer1);
+            customerManager.Delete(customer1);
+            customerManager.Update(customer1);
+
+            Console.ReadLine();
+        }
+    }
+}
